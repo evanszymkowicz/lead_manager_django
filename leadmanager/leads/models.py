288079abffdc4404441f2d11359161fafc57e36b
evanszymkowicz <evan.szymkowicz@evanwolf.co>
2019-04-17 23:43:19 -0400

    migrations

--- leadmanager/leads/models.py
+++ leadmanager/leads/models.py
@@ -3,5 +3,7 @@ from django.db import models
 class Lead(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100, unique=True)
+    message = models.CharField(max_length=500, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     # Don't forget about the cpital T in true
