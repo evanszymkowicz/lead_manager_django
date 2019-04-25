594b2b2b187df9575661923924471e530b0e469b
evanszymkowicz <evan.szymkowicz@evanwolf.co>
2019-04-25 15:12:49 -0400

    frontend part 2 (user auth)

---
+++ leadmanager/frontend/src/components/leads/Dashboard.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Form from "./Form";
+import Leads from "./Leads";
+
+export defualt function Dashboard() {
+	return(
+		<Fragment>
+      <Form />
+      <Leads />
+    </Fragment>
+  );
+}
